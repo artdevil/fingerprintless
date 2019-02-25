377f65bfff10359cd30c0a30f2807eb851a49818
adhiguna <adhiguna.sabril@gmail.com>
2019-02-25 14:01:51 +0700

    update fingerprint for favicon

--- config/initializers/fingerprintless.rb
+++ config/initializers/fingerprintless.rb
@@ -15,6 +15,18 @@ module ActionView
       end
       alias_method_chain :image_tag, :fingerprinting
 
+      def favicon_link_tag_with_fingerprinting(source, options={})
+        fingerprinting = options.delete(:fingerprinting)
+        fingerprinting = true if fingerprinting.nil?
+
+        unless fingerprinting
+          source = favicon_link_tag_without_fingerprinting(source, options)
+          source.sub(/(\-[a-f0-9]+)\.(png|gif|jpg)"/, '.\2"').html_safe
+        else
+          favicon_link_tag_without_fingerprinting(source, options)
+        end
+      end
+      alias_method_chain :favicon_link_tag, :fingerprinting
     end
   end
 end
